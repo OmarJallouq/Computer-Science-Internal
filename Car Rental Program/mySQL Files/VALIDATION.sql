754809c2b6c03f198138d6ec878e25d6bdd4cc26
Omar Jallouq <omarjallouq@gmail.com>
2023-04-15 22:29:24 +0300

    First Commit

--- Car Rental Program/mySQL Files/VALIDATION.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE employee
-ADD CONSTRAINT `employee.email_validation` 
-    CHECK (`email` REGEXP "^[a-zA-Z0-9][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]*?[a-zA-Z0-9._-]?@[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]?\\.[a-zA-Z]{2,63}$");
-    
-ALTER TABLE customer
-ADD CONSTRAINT `customer.email_validation` 
-    CHECK (`email` REGEXP "^[a-zA-Z0-9][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]*?[a-zA-Z0-9._-]?@[a-zA-Z0-9][a-zA-Z0-9._-]*?[a-zA-Z0-9]?\\.[a-zA-Z]{2,63}$");
